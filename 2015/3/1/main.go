d7ceba2d2d9bf74e9895d229648176a889370221
Danilo <danilopavkov@yahoo.com>
2024-10-24 22:27:19 -0700

    Advent 2015 day 3, task 1; sets in golang

---
+++ 2015/3/1/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	content, err := os.ReadFile("input")
+	if err != nil {
+		fmt.Println(err)
+	}
+	santa := coordinates{0, 0}
+	locations := make(map[coordinates]struct{})
+	locations[santa] = struct{}{}
+	for _, c := range content {
+		santa = move(santa, c)
+		locations[santa] = struct{}{}
+		fmt.Println(santa)
+	}
+	fmt.Println(len(locations))
+}
+
+func move(santa coordinates, c byte) coordinates {
+	if c == '^' {
+		return coordinates{santa.X + 1, santa.Y}
+	}
+	if c == 'v' {
+		return coordinates{santa.X - 1, santa.Y}
+	}
+	if c == '<' {
+		return coordinates{santa.X, santa.Y - 1}
+	}
+	if c == '>' {
+		return coordinates{santa.X, santa.Y + 1}
+	}
+	return coordinates{santa.X, santa.Y}
+}
+
+type coordinates struct {
+	X int
+	Y int
+}
