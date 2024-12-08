099f39bc352e90d090178d17c6c2f30b183dd83c
Danilo <danilopavkov@yahoo.com>
2024-12-07 16:01:11 -0800

    Advent 2024 day 7 first puzzle

---
+++ 2024/7/1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("../../../input")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	score := 0
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input := scanner.Text()
+		calibration := parse(input)
+		if isValid(calibration) {
+			score += calibration.result
+		}
+	}
+
+	
+	fmt.Println(score)
+}
+
+func parse(input string) calibration {
+	parts := strings.Split(input, ":")
+	
+	result, _ := strconv.Atoi(parts[0])
+
+	values := []int{}
+
+	trimmedValuesStr := strings.Trim(parts[1], " ")
+	valuesParts := strings.Split(trimmedValuesStr, " ") 
+	for i := 0; i < len(valuesParts); i++ {
+		valueStr := valuesParts[i]
+		value, _ := strconv.Atoi(valueStr)
+		values = append(values, value)
+	}
+
+	return calibration{result, values}
+}
+
+func isValid(calibration calibration) bool {
+	return scoreReached(calibration, 0)
+}
+
+func scoreReached(equation calibration, score int) bool {
+	if (len(equation.values) == 0) {
+		return equation.result == score
+	}
+
+	if (equation.result < score) {
+		return false
+	}
+ 
+	leftover := calibration{equation.result, equation.values[1:]}
+	val := equation.values[0]
+	return scoreReached(leftover, score + val) || scoreReached(leftover, score * val)
+}
+
+type calibration struct {
+	result int
+	values []int
+}
